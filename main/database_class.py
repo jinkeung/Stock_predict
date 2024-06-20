5a8417c495415ca7b5379a1726fc0a46002979a9
Jinkyeong Kim <jinkeung1203@gmail.com>
2024-06-20 16:10:31 +0900

    0620_2

---
+++ main/database_class.py
@@ -0,0 +1,76 @@
+import pymysql
+import pandas as pd
+from datetime import date, timedelta
+import yfinance as yf
+
+# 전체(2년치) 데이터 가져오는 함수
+def get_all_data(stock_code, stock_name):
+    # MySQL 연결 설정
+    host = '127.0.0.1'
+    port = 3306
+    username = 'root'
+    password = '1234'
+    database = 'stock_predict'
+
+    # MySQL 연결
+    connection = pymysql.connect(host=host,port=port,user=username,password=password,database=database)
+
+    # 날짜 설정
+    end_date = date.today()
+    start_date = end_date - timedelta(days=730)
+
+    # 종목 코드와 이름 설정
+    stock_code = ["035420.KS", "005930.KS"]
+    stock_name = ["네이버", "삼성전자"]
+
+    try:
+        # 각 종목별로 데이터 다운로드 및 MySQL에 저장
+        for idx, code in enumerate(stock_code):
+            stock_data = yf.download(code, start=start_date, end=end_date)
+
+
+            # 테이블 생성 (이미 존재할 경우 무시)
+            with connection.cursor() as cursor:
+
+                create_table_query = f"""
+                CREATE TABLE IF NOT EXISTS {stock_name[idx]} (
+                    Date DATE PRIMARY KEY,
+                    Open FLOAT,
+                    High FLOAT,
+                    Low FLOAT,
+                    Close FLOAT,
+                    Adj_Close FLOAT,
+                    Volume INT
+                );
+                """
+
+                cursor.execute(create_table_query)
+
+            # 데이터프레임을 MySQL 테이블에 삽입
+            with connection.cursor() as cursor:
+                truncate_table_query = f"TRUNCATE TABLE {stock_name[idx]};"
+                cursor.execute(truncate_table_query)
+                for index, row in stock_data.iterrows():
+                    sql = f"""
+                    INSERT INTO {stock_name[idx]} (Date, Open, High, Low, Close, Adj_Close, Volume)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)
+                    ON DUPLICATE KEY UPDATE
+                    Open=VALUES(Open),
+                    High=VALUES(High),
+                    Low=VALUES(Low),
+                    Close=VALUES(Close),
+                    Adj_Close=VALUES(Adj_Close),
+                    Volume=VALUES(Volume);
+                    """
+                    cursor.execute(sql, (index.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Adj Close'], row['Volume']))
+
+            # 변경 사항 커밋
+            connection.commit()
+
+        print("주식 데이터가 MySQL 데이터베이스에 저장되었습니다.")
+    except Exception as e:
+        print(e)
+        pass
+    finally:
+        # 연결 종료
+        connection.close()
