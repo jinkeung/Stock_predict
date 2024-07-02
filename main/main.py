517ed76d0d9dacd42c9e26f46304b5a650a99b4d
Jinkyeong Kim <jinkeung1203@gmail.com>
2024-07-02 16:53:47 +0900

    0702

--- main/main.py
+++ main/main.py
@@ -169,78 +169,80 @@ def stock():
         graph_data_df=db.return_graph_data(stock_name)
         df_data, df_future_data = learn.machine_learning(stock_name)
 
-        if not(graph_data_df.empty) and not(df_data.empty):
-            if not(graph_data_df.empty):
-                try:
-                    # 주식 실질적 그래프
-                    with stock_graph:
 
-                        st.subheader(f'{stock_name} 실제 주가')
-                        st.sidebar.markdown('---')
+        if not(graph_data_df.empty):
+            try:
+                # 주식 실질적 그래프
+                with stock_graph:
 
-                        candlestick=go.Candlestick(x=graph_data_df['날짜'],open=graph_data_df['시가'],
-                                                high=graph_data_df['고가'], low=graph_data_df['저가'], close=graph_data_df['종가'])
-                        line=go.Scatter(x=graph_data_df["날짜"], y=graph_data_df["종가"],
-                                        mode="lines", name="종가")
-                        graph_type=st.sidebar.radio("차트 종류를 선택하세요",("Candle_stick","Line"))
+                    st.subheader(f'{stock_name} 실제 주가')
+                    st.sidebar.markdown('---')
 
-                        if graph_type=="Candle_stick": fig=go.Figure(candlestick)
-                        elif graph_type=="Line": fig=go.Figure(line)
-                        st.plotly_chart(fig)
-                    # 주식 뉴스 리스트
-                    with news_list:
-                        if news_df.empty == False:
-                            table_data = []
-                            for index, row in news_df.iterrows():
-                                link = f"<a href='{row['주소']}' target='_blank'>{row['제목']}</a>"
-                                table_data.append([link])
-                            link_df = pd.DataFrame(table_data, columns=['뉴스 데이터'])
-                            st.write(link_df.to_html(escape=False, index=False), unsafe_allow_html=True)
-                        else:
-                            table_data = []
-                            for index, row in news_df.iterrows():
-                                link = f"<a href='{row['주소']}' target='_blank'>{row['제목']}</a>"
-                                table_data.append([link])
-                            link_df = pd.DataFrame(table_data, columns=['뉴스 데이터'])
-                            st.write(link_df.to_html(escape=False, index=False), unsafe_allow_html=True)
-                            st.write("최근 1년 내 검색된 관련뉴스가 없습니다")
-                    # 주식 상세 데이터
-                    with stock_real_list_container:
-                        st.subheader('주식 상세 데이터')
-                        col1,col2 = st.columns([1,1])
-                        stock_real_list1 = col1
-                        col2.empty()
-                        stock_table = db.return_show_data(stock_name)
-                        st.dataframe(stock_table,height=400, width=2000)
-                except Exception as e:
-                    Log.error(f"주식 데이터에서 예외 발생했습니다 : {e}")
-                    st.experimental_rerun()
-            else:
-                Log.error("주식 기본 데이터가 너무 적습니다")
-                st.subheader("상세 주식 데이터")
-                st.write("기본 정보 제공을 위한 데이터가 너무 적습니다.")
-            # 예측 데이터 출력
-            if not(df_data.empty):
-                try:
-                    df_future_data['Date'] = pd.to_datetime(df_future_data['Date']).dt.date
-                    df_future_data['Predicted Price'] = df_future_data['Predicted Price'].round(-1).astype(int)
+                    candlestick=go.Candlestick(x=graph_data_df['날짜'],open=graph_data_df['시가'],
+                                            high=graph_data_df['고가'], low=graph_data_df['저가'], close=graph_data_df['종가'])
+                    line=go.Scatter(x=graph_data_df["날짜"], y=graph_data_df["종가"],
+                                    mode="lines", name="종가")
 
-                    # 예측 그래프
-                    with stock_predict_graph:
-                        st.subheader(f'{stock_name} 예측 주가 [ 30day ]')
-                        st.line_chart(df_future_data.set_index('Date')['Predicted Price'])
-                    # 예측 데이터
-                    with stock_predict_list:
-                        st.subheader(f'{stock_name} 예측 데이터')
-                        st.dataframe(df_future_data,height=310, width=400)
-                except Exception as e:
-                    Log.error(f"예측 데이터에서 예외가 발생되었습니다 : {e}")
-                    st.experimental_rerun()
-            else:
-                st.subheader("예측 주식 데이터")
-                st.write("예측 정보 제공을 위한 데이터가 너무 적습니다.")
+                    graph_type=st.sidebar.radio("차트 종류를 선택하세요",("Candle_stick","Line"))
+
+                    if graph_type=="Candle_stick": fig=go.Figure(candlestick)
+                    elif graph_type=="Line": fig=go.Figure(line)
+
+                    fig.update_layout(yaxis=dict(tickformat=','))
+                    st.plotly_chart(fig)
+                # 주식 뉴스 리스트
+                with news_list:
+                    if news_df.empty == False:
+                        table_data = []
+                        for index, row in news_df.iterrows():
+                            link = f"<a href='{row['주소']}' target='_blank'>{row['제목']}</a>"
+                            table_data.append([link])
+                        link_df = pd.DataFrame(table_data, columns=['뉴스 데이터'])
+                        st.write(link_df.to_html(escape=False, index=False), unsafe_allow_html=True)
+                    else:
+                        table_data = []
+                        for index, row in news_df.iterrows():
+                            link = f"<a href='{row['주소']}' target='_blank'>{row['제목']}</a>"
+                            table_data.append([link])
+                        link_df = pd.DataFrame(table_data, columns=['뉴스 데이터'])
+                        st.write(link_df.to_html(escape=False, index=False), unsafe_allow_html=True)
+                        st.write("최근 1년 내 검색된 관련뉴스가 없습니다")
+                # 주식 상세 데이터
+                with stock_real_list_container:
+                    st.subheader('주식 상세 데이터')
+                    col1,col2 = st.columns([1,1])
+                    stock_real_list1 = col1
+                    col2.empty()
+                    stock_table = db.return_show_data(stock_name)
+                    st.dataframe(stock_table,height=400, width=2000)
+            except Exception as e:
+                Log.error(f"주식 데이터에서 예외 발생했습니다 : {e}")
+                st.experimental_rerun()
         else:
-            st.subheader("현재 주식 정보 제공에 대한 데이터가 적습니다")
+            Log.error("주식 기본 데이터가 너무 적습니다")
+            st.subheader("상세 주식 데이터")
+            st.write("기본 정보 제공을 위한 데이터가 너무 적습니다.")
+        # 예측 데이터 출력
+        if not(df_data.empty):
+            try:
+                df_future_data['Date'] = pd.to_datetime(df_future_data['Date']).dt.date
+                df_future_data['Predicted Price'] = df_future_data['Predicted Price'].round(-1).astype(int)
+
+                # 예측 그래프
+                with stock_predict_graph:
+                    st.subheader(f'{stock_name} 예측 주가 [ 30day ]')
+                    st.line_chart(df_future_data.set_index('Date')['Predicted Price'])
+                # 예측 데이터
+                with stock_predict_list:
+                    st.subheader(f'{stock_name} 예측 데이터')
+                    st.dataframe(df_future_data,height=310, width=400)
+            except Exception as e:
+                Log.error(f"예측 데이터에서 예외가 발생되었습니다 : {e}")
+                st.experimental_rerun()
+        else:
+            st.subheader("예측 주식 데이터")
+            st.write("예측 정보 제공을 위한 데이터가 너무 적습니다.")
+
     # 검색 버튼
     search_button=st.sidebar.button('검색하기')
     if search_button:
