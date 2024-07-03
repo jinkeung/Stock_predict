6a2dd6c81a1bd9af9c9a886e7fda55f8b10f4173
JinKyeong Kim <jinkeung1203@gmail.com>
2024-07-03 20:00:27 +0900

    0701

--- main/database_class.py
+++ main/database_class.py
@@ -72,29 +72,6 @@ def set_user_data(join_id, join_pwd, join_name):
             con.close()
 
 
-# 회원정보 데이터베이스 적재 함수
-def set_user_data(join_id, join_pwd, join_name):
-    u_salt = bcrypt.gensalt()
-    pepper = st.secrets["database"]["pepper"]
-    hash_pwd = bcrypt.hashpw((join_pwd + pepper).encode(), salt=u_salt)
-
-    try:
-        con = connect_db()
-        cursor = con.cursor()
-        query = '''INSERT INTO USER_DATA (U_ID, U_PWD, U_NAME, U_SALT) VALUES (%s, %s, %s, %s)'''
-        cursor.execute(query, (join_id, hash_pwd, join_name, u_salt))  # 튜플 형태로 파라미터 전달
-        con.commit()
-        join_success = True
-        return join_success
-    except Exception as e:
-        Log.error(f"회원가입 중 예외가 발생했습니다 : {e}")
-        join_success = False
-        return join_success
-    finally:
-        if cursor:
-            cursor.close()
-        if con:
-            con.close()
 
 
 # 전체 데이터베이스 적재
